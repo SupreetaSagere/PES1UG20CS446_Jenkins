038ea73a179c52099832301b88a5cbb03e3aa01a
SupreetaSagere <85924841+SupreetaSagere@users.noreply.github.com>
2023-02-17 21:03:34 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(void) {
-  cout<< "PES1UG20CS446_SupreetaSagere\n"
+  cout<< "PES1UG20CS446_SupreetaSagere\n";
   cout << "Creating my own pipeline and printing SRN and name";
   return 0;
 }
