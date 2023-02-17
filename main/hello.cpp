829e369454ae1f720f74d97dcae350dc97475238
SupreetaSagere <85924841+SupreetaSagere@users.noreply.github.com>
2023-02-17 20:45:32 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World from PES1UG20CS446\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
